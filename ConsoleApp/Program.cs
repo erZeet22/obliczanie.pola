eb1c5ed4023bc14034cc682676d714cf08a7a366
erzeet <kazmirz0911@gmail.com>
2022-03-18 22:53:34 +0100

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string r;
+            double conv;
+
+            Console.Write("Podaj średnice twojego koła: ");
+            
+            r = Console.ReadLine();
+
+            conv = Convert.ToInt32(r);
+
+            double wynik = Math.PI * (conv * conv);
+
+
+            Console.WriteLine($"Pole twojego koła wynosi {Math.Round(wynik)}");
+
+        }
+    }
+}
